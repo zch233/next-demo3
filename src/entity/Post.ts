1a86adbee297011608420e578ed6bf713cf7ceab
zhangchao <529743595@qq.com>
2020-07-21 23:15:04 +0800

    entity:create And Resolve  syntax 'decorators-legacy' Bug

---
+++ src/entity/Post.ts
@@ -0,0 +1,17 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm';
+
+@Entity('posts')
+export class Post {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @Column('varchar')
+  title: string;
+  @Column('text')
+  content: string;
+  @Column('int')
+  authorId: number;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
